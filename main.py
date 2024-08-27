20226ef6e4b864e8aea41c9073cdbc328f48b530
Xinyu Ma <kinu@meta.com>
2024-08-26 21:54:55 -0700

    Add implicit flow

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from aiohttp import web
 import jwt
 
 
+IMPLICIT_FLOW = False
+RESPONSE_TYPE = 'id_token' if IMPLICIT_FLOW else 'code'
+
 # Client ID from project "usable-auth-library", configured for
 # general purpose API testing
 CLIENT_ID = '764086051850-6qr4p6gpi6hn506pt8ejuq83di341hur.apps.googleusercontent.com'
@@ -14,11 +17,12 @@ CLIENT_SECRET = 'd-FL95Q19q7MQmFpd7hHD0Ty'
 URL = (f'https://accounts.google.com/o/oauth2/v2/auth?'
        f'scope=openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email'
        '+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&'
-       f'redirect_uri=http%3A%2F%2Flocalhost%3A8085%2F&'
-       f'response_type=code&'
+       f'redirect_uri=http%3A%2F%2Flocalhost%3A8085&'
+       f'response_type={RESPONSE_TYPE}&'
        f'client_id={CLIENT_ID}&'
        f'nonce=MCowBQYDK2VwAyEA6qIwi5SOV3fj0T/Rqpi6lbqDh1hF93BIDG/2zkjG9Ec=&'
-       f'access_type=offline')
+       f'state=abcd1234&'
+       f'access_type={"online" if IMPLICIT_FLOW else "offline"}')
 # Obtained from 'https://accounts.google.com/.well-known/openid-configuration#jwks_uri'
 GOOGLE_KEY_URL = 'https://www.googleapis.com/oauth2/v3/certs'
 
@@ -27,7 +31,7 @@ async def exchange_token(code: str):
     async with aiohttp.ClientSession() as session:
         data = {
             'code': code,
-            'redirect_uri': 'http://localhost:8085/',
+            'redirect_uri': 'http://localhost:8085',
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
             'scope': 'openid',
@@ -62,17 +66,26 @@ async def exchange_token(code: str):
 
 async def handler(request: web.Request):
     query = request.query
-    print('code=', query['code'])
-    aio.create_task(exchange_token(query['code']))
-    return web.Response(text="Done")
+    if IMPLICIT_FLOW:
+        return web.Response(
+        text=('<html><head><title>Done</title></head><body>'
+              '<script>document.write(document.location.hash);</script>'
+              '</body></html>'),
+        content_type='text/html')
+    else:
+        print('code=', query['code'])
+        aio.create_task(exchange_token(query['code']))
+        return web.Response(text="Done")
 
 
 def main():
     print(URL)
     if sys.platform == "darwin":
         os.spawnlp(os.P_NOWAIT, 'open', 'open', URL)
-    else:
+    elif sys.platform == "linux":
         os.spawnlp(os.P_NOWAIT, 'xdg-open', 'xdg-open', URL)
+    else:
+        os.startfile(URL)
 
     app = web.Application()
     app.add_routes([web.get('/', handler)])
